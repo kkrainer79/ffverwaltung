a76f78f1a936514672862615ac04d69889d29e5d
kkrainer79 <58879413+kkrainer79@users.noreply.github.com>
2024-01-08 20:15:34 +0100

    Development-Database registered

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,9 @@
 import {initializeApp} from "firebase/app";
 
+/*
+PRODUCTIVE-DATABASE
+START
+*/
 const firebaseConfig = {
   apiKey: "AIzaSyAuzBFq1XkoaKCD4FE38dB3O2Zu0qSvHHU",
   authDomain: "ffpservice-c01a9.firebaseapp.com",
@@ -9,6 +13,29 @@ const firebaseConfig = {
   messagingSenderId: "1082971691323",
   appId: "1:1082971691323:web:9de10ffb2c6caf3ba8ff1e"
 }
+/*
+-----> END <-----*/
+
+
+
+/*
+DEVELOPMENT-DATABASE
+-----> START <-----
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOk0zxaelfz0bPGWlU8dduDdVegzdJV0Y",
+  authDomain: "ffpservicedev.firebaseapp.com",
+  projectId: "ffpservicedev",
+  storageBucket: "ffpservicedev.appspot.com",
+  messagingSenderId: "77065195531",
+  appId: "1:77065195531:web:ffeee0d778e51b1a88b70e"
+};
+
+-----> END <-----
+*/
+
+
+
 
 const app = initializeApp(firebaseConfig);
 
